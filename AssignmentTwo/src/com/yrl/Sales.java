b5e08a86bb55155806c60c760fccb1ceb6058507
noahbustard <nbustard2@unl.edu>
2024-02-27 16:06:43 -0600

    New Classes

---
+++ AssignmentTwo/src/com/yrl/Sales.java
@@ -0,0 +1,30 @@
+package com.yrl;
+
+public class Sales {
+	private String saleCode;
+	private String storeCode;
+	private String customerCode;
+	private String salespersonCode;
+	private String date;
+
+	public String getSaleCode() {
+		return this.saleCode;
+	}
+
+	public String getStoreCode() {
+		return this.storeCode;
+	}
+
+	public String getCustomerCode() {
+		return this.customerCode;
+	}
+
+	public String getSalespersonCode() {
+		return this.salespersonCode;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+}
