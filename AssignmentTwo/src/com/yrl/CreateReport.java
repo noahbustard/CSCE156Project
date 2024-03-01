26a18520d2b307869ca228c97cc0e590ce021ee7
noahbustard <nbustard2@unl.edu>
2024-03-01 10:25:37 -0600

    Inconsistent merge state: could not find file MERGE_MSG in .git folder. This file contains the commit message for a merge commit.

---
+++ AssignmentTwo/src/com/yrl/CreateReport.java
@@ -0,0 +1,15 @@
+package com.yrl;
+
+public class CreateReport {
+	public static void getTotalReport() {
+		return;
+	}
+
+	public static void getStoreReport() {
+
+	}
+
+	public static void getSalesReport() {
+
+	}
+}
