5d8a94cf1fb46ee16173c137165cc38d8cc99ea0
noahbustard <nbustard2@unl.edu>
2024-03-27 19:02:51 -0500

    Edits

--- Database.sql
+++ Database.sql
@@ -1,6 +1,8 @@
 use nbustard2;
 
 
+drop table if exists SaleItem;
+drop table if exists Item;
 drop table if exists Sale;
 drop table if exists Person;
 drop table if exists Store;
@@ -28,7 +30,8 @@ firstName varchar(256) not null,
 lastName varchar(256) not null,
 storeId int,
 addressId int,
-foreign key (storeId) references Store(storeId)
+foreign key (storeId) references Store(storeId),
+foreign key (addressId) references Address(addressId)
 );
 
 create table Sale (
@@ -43,9 +46,30 @@ foreign key (customerId) references Person(personId),
 foreign key (salespersonId) references Person(personId)
 );
 
+create table Item(
+itemId int not null primary key auto_increment,
+itemCode varchar(4),
+type varchar(1),
+name varchar(256),
+baseCost double
+);
 
-alter table Store
-add foreign key (addressId) references Address(addressId);
+create table SaleItem (
+saleCode varchar(4),
+itemCode varchar(4),
+startDate varchar(10),
+endDate varchar(10),
+hoursBilled double,
+servicemanId int,
+gbsPurchased double,
+phoneNumber varchar(12),
+daysPurchased int,
+itemId int not null,
+saleId int not null,
+foreign key (itemId) references Item(itemId),
+foreign key (saleId) references Sale(saleId)
+);
+
 
 
 
